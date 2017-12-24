48c4f661e7cf866e312c8815526d91d4cd87f4e5
Josh Leder <slushie@gmail.com>
2017-12-24 00:01:46 -0600

    pouch to vue

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import axios from 'axios'
 import debug from 'debug'
+import { remote } from 'electron'
+import PouchDB from 'pouchdb'
 
 import App from './App'
 import router from './router'
@@ -17,12 +19,16 @@ if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
-/**
- * Write to debug log as `kubist:component:${name}`
- *
- * @param {string} fmt
- * @param {...*} [varargs]
- */
+const apiUrl = remote.getGlobal('apiUrl')
+const pouch = PouchDB.defaults({
+  prefix: apiUrl + '/db'
+})
+
+pouch.plugin(require('pouchdb-find'))
+pouch.plugin(require('pouchdb-live-find'))
+
+Vue.use(require('vue-pouch'), { pouch })
+
 Vue.prototype.$debug = function (fmt, ...varargs) {
   const name = this.constructor.options.name
   debug(`kubist:component:${name}`)
