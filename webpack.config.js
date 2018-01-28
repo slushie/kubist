af0bde110fa4629bc02f9c0734c5fc3aae11b29f
Brad Janke <brad.janke@gmail.com>
2018-01-28 16:38:38 -0600

    Adds index.html generation.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js', // entry point of application
   plugins: [
-    new CleanWebpackPlugin(['dist']), // cleans dist folder
+    new HtmlWebpackPlugin({title: 'Kubist'}), // generates index.html in our dist dir
+    new CleanWebpackPlugin(['dist']) // cleans dist dir
   ],
   output: {
     filename: 'bundle.js', // output file name
