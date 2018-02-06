46e180eeeea38a787a51e2b6406da62a2f9c00c5
Josh Leder <josh@ha.cr>
2018-02-05 18:36:23 -0600

    stop throttling redraw.

--- src/docs/query.js
+++ src/docs/query.js
@@ -3,11 +3,7 @@ import _ from 'lodash'
 
 import { getLocalDB } from './db'
 
-export const REDRAW_INTERVAL = 100
-
-const redraw = _.throttle(m.redraw, REDRAW_INTERVAL)
-
 export function liveFind (query) {
   return getLocalDB().liveFind(query)
-    .on('update', () => setImmediate(redraw))
+    .on('update', () => setImmediate(m.redraw))
 }
